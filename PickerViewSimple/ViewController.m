91b9c4810c84fab43d05055541dd79e1a195c2da
heqiang <heqiang1@cmcm.com>
2019-04-26 12:52:44 +0800

    add files

--- PickerViewSimple/ViewController.m
+++ PickerViewSimple/ViewController.m
@@ -1,14 +1,11 @@
-//
-//  ViewController.m
-//  PickerViewSimple
-//
-//  Created by 叶一 on 2019/4/26.
-//  Copyright © 2019 heqiang. All rights reserved.
-//
 
 #import "ViewController.h"
 
-@interface ViewController ()
+@interface ViewController ()<UIPickerViewDelegate,UIPickerViewDataSource>
+@property (strong, nonatomic) IBOutlet UIDatePicker *datePicker;
+@property (strong, nonatomic) IBOutlet UILabel *labelView;
+@property (strong, nonatomic) IBOutlet UIButton *pickerButton;
+
 
 @end
 
@@ -17,6 +14,20 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    //给按钮添加点击事件
+    [self.pickerButton addTarget:self action:@selector(onClickButton:) forControlEvents:UIControlEventTouchDown];
+}
+
+
+- (IBAction)onClickButton:(id)sender {
+    NSDate *date = self.datePicker.date;//取出UIDatePicker的时间
+    NSLog(@"The date is:%@",[date descriptionWithLocale:[NSLocale currentLocale]]);
+    NSDateFormatter *dateFormat = [[NSDateFormatter alloc] init];
+    dateFormat.dateFormat = @"YYYY-MM-dd HH:mm:ss";
+    NSString *dateString = [dateFormat stringFromDate:date];
+    NSLog(@"formate date is:%@", dateString);
+    self.labelView.text = dateString;
 }
 
 
